3f4cc14e3b4d8dcaf5a7dc618e8e233bd33f768f
maeyan <y.maeyan@gmail.com>
2014-11-21 01:20:44 +0900

    フォーム部分と検索部分でPanelを分離

--- FileSearchApp/FileSearchApp/view/fmFileSearch.Designer.cs
+++ FileSearchApp/FileSearchApp/view/fmFileSearch.Designer.cs
@@ -26,44 +26,42 @@
             this.components = new System.ComponentModel.Container();
             this.flp_SearchResult = new System.Windows.Forms.FlowLayoutPanel();
             this.pnl_form = new System.Windows.Forms.Panel();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pb_Search = new System.Windows.Forms.PictureBox();
             this.tb_SearchWord = new System.Windows.Forms.TextBox();
             this.pb_TargetFolderPath = new System.Windows.Forms.PictureBox();
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
-            this.flp_SearchResult.SuspendLayout();
             this.pnl_form.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Search)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_TargetFolderPath)).BeginInit();
             this.SuspendLayout();
             // 
             // flp_SearchResult
             // 
-            this.flp_SearchResult.Controls.Add(this.pnl_form);
-            this.flp_SearchResult.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flp_SearchResult.Location = new System.Drawing.Point(0, 0);
+            this.flp_SearchResult.Location = new System.Drawing.Point(1, 95);
             this.flp_SearchResult.Name = "flp_SearchResult";
-            this.flp_SearchResult.Size = new System.Drawing.Size(784, 462);
+            this.flp_SearchResult.Size = new System.Drawing.Size(783, 366);
             this.flp_SearchResult.TabIndex = 0;
             // 
             // pnl_form
             // 
-            this.pnl_form.Controls.Add(this.pictureBox3);
+            this.pnl_form.Controls.Add(this.pb_Search);
             this.pnl_form.Controls.Add(this.tb_SearchWord);
             this.pnl_form.Controls.Add(this.pb_TargetFolderPath);
-            this.pnl_form.Location = new System.Drawing.Point(3, 3);
+            this.pnl_form.Location = new System.Drawing.Point(1, 1);
             this.pnl_form.Name = "pnl_form";
-            this.pnl_form.Size = new System.Drawing.Size(769, 100);
+            this.pnl_form.Size = new System.Drawing.Size(783, 93);
             this.pnl_form.TabIndex = 0;
             // 
-            // pictureBox3
+            // pb_Search
             // 
-            this.pictureBox3.Image = global::FileSearchApp.Properties.Resources.search;
-            this.pictureBox3.Location = new System.Drawing.Point(559, 36);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(60, 38);
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
-            this.toolTip.SetToolTip(this.pictureBox3, "検索");
+            this.pb_Search.Image = global::FileSearchApp.Properties.Resources.search;
+            this.pb_Search.Location = new System.Drawing.Point(559, 36);
+            this.pb_Search.Name = "pb_Search";
+            this.pb_Search.Size = new System.Drawing.Size(60, 38);
+            this.pb_Search.TabIndex = 3;
+            this.pb_Search.TabStop = false;
+            this.toolTip.SetToolTip(this.pb_Search, "検索");
+            this.pb_Search.Click += new System.EventHandler(this.pb_Search_Click);
             // 
             // tb_SearchWord
             // 
@@ -96,6 +94,7 @@
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(784, 462);
             this.Controls.Add(this.flp_SearchResult);
+            this.Controls.Add(this.pnl_form);
             this.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
@@ -103,10 +102,9 @@
             this.MinimumSize = new System.Drawing.Size(800, 500);
             this.Name = "fmFileSearch";
             this.Text = "ファイル検索";
-            this.flp_SearchResult.ResumeLayout(false);
             this.pnl_form.ResumeLayout(false);
             this.pnl_form.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Search)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_TargetFolderPath)).EndInit();
             this.ResumeLayout(false);
 
@@ -117,7 +115,7 @@
         private System.Windows.Forms.FlowLayoutPanel flp_SearchResult;
         private System.Windows.Forms.Panel pnl_form;
         private System.Windows.Forms.PictureBox pb_TargetFolderPath;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pb_Search;
         private System.Windows.Forms.TextBox tb_SearchWord;
         private System.Windows.Forms.ToolTip toolTip;
     }
